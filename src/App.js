ade4fa0b83e2508a579d5609aa121a7de01c09c5
aiswarya8110 <saish8110@gmail.com>
2023-06-12 20:20:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import persons from './data';
+import React, { useState } from 'react';
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text } = persons[index];
+
+    function randomPersonHandler(){
+      let randomIndex = Math.floor(Math.random() * persons.length);
+      setIndex(randomIndex);
+    }
+
+    function previousPersonHandler(){
+      setIndex((prevState)=>{
+        let prevIndex = prevState - 1;
+        if(prevIndex < 0){
+          prevIndex = persons.length - 1;
+        }
+
+        return prevIndex;
+      })
+    }
+
+    function nextPersonHandler(){
+      setIndex((prevState)=>{
+        let nextIndex = prevState + 1;
+        if(nextIndex > persons.length - 1){
+          nextIndex = 0;
+        }
+
+        return nextIndex;
+      })
+    }
+
+    console.log(name);
+    return (
+      <main>
+        <article className="review">
+          <div className="img-container">
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+          <div className="btn-container">
+            <button className="prev-btn" onClick={previousPersonHandler}>
+              <FaChevronLeft />
+            </button>
+            <button className="next-btn" onClick={nextPersonHandler}>
+              <FaChevronRight />
+            </button>
+          </div>
+          <button className="random-btn" onClick={randomPersonHandler}>
+            surprise me!
+          </button>
+        </article>
+      </main>
+    )
 }
 
 export default App;
